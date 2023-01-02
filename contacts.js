2bfcf8b40f082a9bf48c9cf2ceac626bbacea5a2
Daria Ośka-Ossowska <daria.oska@gmail.com>
2023-01-02 19:08:26 +0100

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -8,19 +8,7 @@ const contactsPath = path.join(__dirname, 'db', 'contacts.json')
 
 // TODO: udokumentuj każdą funkcję
 const listContacts = async () => {
-		const contacts = await fs.readdir(__dirname)
-		const filesStats = await Promise.all(
-			files.map(async filename => {
-				const stats = await fs.stat(filename)
-				return {
-					name: filename,
-					size: stats.size,
-					date: stats.mtime,
-					isFolder: stats.isDirectory(),
-				}
-			})
-		)
-		console.log(filesStats)
+
 }
 
 function getContactById(contactId) {
